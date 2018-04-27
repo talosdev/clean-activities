3adf91afc90af4dfc37a066d7ac1e2715c95f54b
Aris Papadopoulos <aris@futureworkshops.com>
2018-04-27 15:17:10 +0200

    Created feature packages and custom Application

---
+++ app/src/main/java/talosdev/permission/ThisApplication.java
@@ -0,0 +1,9 @@
+package talosdev.permission;
+
+import android.app.Application;
+
+/*
+ * Copyright (c) 2018 FutureWorkshops. All rights reserved.
+ */
+public class ThisApplication extends Application {
+}
