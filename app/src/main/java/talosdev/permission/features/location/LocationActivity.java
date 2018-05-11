9e1adfdf8ec3ca2ea2117b5eb39195b1b849c484
talosdev <aristides.papadopoulos@gmail.com>
2018-05-11 18:12:11 +0200

    Standard handling of permission request and location values in the Activity

--- app/src/main/java/talosdev/permission/features/location/LocationActivity.java
+++ app/src/main/java/talosdev/permission/features/location/LocationActivity.java
@@ -1,10 +1,21 @@
 package talosdev.permission.features.location;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import javax.inject.Inject;
 
@@ -16,6 +27,8 @@ import talosdev.permission.R;
 public class LocationActivity extends AppCompatActivity implements LocationContract.View {
 
 
+    private static final int REQ_CODE = 111;
+
     @BindView(R.id.latitudeTextView)
     TextView latitudeTextView;
 
@@ -24,6 +37,8 @@ public class LocationActivity extends AppCompatActivity implements LocationContr
 
     @Inject
     LocationContract.Presenter presenter;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
 
     public static Intent newIntent(Context context) {
@@ -41,6 +56,61 @@ public class LocationActivity extends AppCompatActivity implements LocationContr
         setContentView(R.layout.location_activity);
 
         ButterKnife.bind(this);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            getLocation();
+        } else {
+            requestPermission();
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    @SuppressWarnings({"MissingPermission"})
+    private void getLocation() {
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            presenter.onLocationAvailable(location.getLatitude(), location.getLongitude());
+                        } else {
+                            //TODO
+                        }
+                    }
+                });
+    }
+
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            // TODO
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQ_CODE);
+
+        }
+    }
+
+
+    @Override
+    @SuppressWarnings({"MissingPermission"})
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQ_CODE &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getLocation();
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
