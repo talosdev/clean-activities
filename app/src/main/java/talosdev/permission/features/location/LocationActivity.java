48b375392b9abe48fc85f4e55458ac77ced76b9d
talosdev <aristides.papadopoulos@gmail.com>
2018-05-12 12:23:42 +0200

    Added warnings and handling permissions request logic

--- app/src/main/java/talosdev/permission/features/location/LocationActivity.java
+++ app/src/main/java/talosdev/permission/features/location/LocationActivity.java
@@ -5,22 +5,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresPermission;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import dagger.android.AndroidInjection;
 import talosdev.permission.R;
 
@@ -34,6 +41,18 @@ public class LocationActivity extends AppCompatActivity implements LocationContr
 
     @BindView(R.id.longitudeTextView)
     TextView longitudeTextView;
+
+    @BindView(R.id.softDenyTextView)
+    TextView softDeniedWarningTextView;
+
+    @BindView(R.id.hardDenyTextView)
+    TextView hardDeniedWarningTextView;
+
+    @BindViews({R.id.softDenyTextView, R.id.hardDenyTextView})
+    List<TextView> deniedTextViews;
+
+    private static ButterKnife.Action<View> VISIBLE = (v, index) -> v.setVisibility(View.VISIBLE);
+    private static ButterKnife.Action<View> GONE = (v, index) -> v.setVisibility(View.GONE);
 
     @Inject
     LocationContract.Presenter presenter;
@@ -61,14 +80,25 @@ public class LocationActivity extends AppCompatActivity implements LocationContr
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             getLocation();
         } else {
             requestPermission();
         }
+    }
+
+
+    @Override
+    public void showLatitude(String latitude) {
+        latitudeTextView.setText(latitude);
+    }
+
+    @Override
+    public void showLongitude(String longitude) {
+        longitudeTextView.setText(longitude);
     }
 
     @RequiresPermission(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -88,38 +118,70 @@ public class LocationActivity extends AppCompatActivity implements LocationContr
     }
 
     private void requestPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)) {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQ_CODE);
 
-            // TODO
-
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQ_CODE);
-
-        }
     }
 
 
     @Override
     @SuppressWarnings({"MissingPermission"})
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQ_CODE &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            getLocation();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQ_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                hidePermissionDeniedWarning();
+                getLocation();
+            } else {
+                handlePermissionDenied();
+            }
         } else {
             super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
-    @Override
-    public void showLatitude(String latitude) {
-        latitudeTextView.setText(latitude);
+
+    private void handlePermissionDenied() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            showSoftPermissionDeniedWarning();
+        } else {
+            // user has checked the "Do not ask again" checkbox
+            showHardPermissionDeniedWarning();
+        }
     }
 
-    @Override
-    public void showLongitude(String longitude) {
-        longitudeTextView.setText(longitude);
+    private void hidePermissionDeniedWarning() {
+        ButterKnife.apply(deniedTextViews, GONE);
     }
+
+    private void showSoftPermissionDeniedWarning() {
+        ButterKnife.apply(softDeniedWarningTextView, VISIBLE);
+        ButterKnife.apply(hardDeniedWarningTextView, GONE);
+    }
+
+
+    private void showHardPermissionDeniedWarning() {
+        ButterKnife.apply(hardDeniedWarningTextView, VISIBLE);
+        ButterKnife.apply(softDeniedWarningTextView, GONE);
+    }
+
+    @OnClick(R.id.softDenyTextView)
+    public void softDenyTextViewClicked(View view) {
+        requestPermission();
+    }
+
+    @OnClick(R.id.hardDenyTextView)
+    public void hardDenyTextViewClicked(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("package", getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
+    }
+
 }
+
+
+
